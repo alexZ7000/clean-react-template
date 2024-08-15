bb8e9e8953bd0acad905d01b362ae8b4776a5321
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2024-08-15 09:03:30 -0300

    adding tsconfig.app to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,7 @@ module.exports = {
     parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
-        project: ["./tsconfig.json", "./tsconfig.node.json"],
+        project: ["./tsconfig.app.json", "./tsconfig.node.json"],
         tsconfigRootDir: __dirname
     },
     overrides: [
