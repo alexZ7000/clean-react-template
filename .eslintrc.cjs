eb22f60c6e9b73cb0f33e04765d8c9bd88a334ea
Alessandro Lima <78627928+alexZ7000@users.noreply.github.com>
2024-08-25 12:01:19 -0300

    enable eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -38,16 +38,6 @@ module.exports = {
     parser: "@typescript-eslint/parser",
     plugins: ["react", "react-refresh"],
     rules: {
-        "@typescript-eslint/no-unsafe-member-access": "warn",
-        "@typescript-eslint/no-unsafe-assignment": "warn",
-        "@typescript-eslint/no-unsafe-call": "warn",
-        "@typescript-eslint/no-misused-promises": "warn",
-        "@typescript-eslint/no-unsafe-return": "warn",
-        "@typescript-eslint/no-floating-promises": "warn",
-        "@typescript-eslint/no-explicit-any": "warn",
-        "@typescript-eslint/no-unsafe-argument": "warn",
-        "@typescript-eslint/no-unsafe-enum-comparison": "warn",
-        "@typescript-eslint/no-unused-vars": "warn",
         "react-refresh/only-export-components": [
             "warn",
             { allowConstantExport: true }
